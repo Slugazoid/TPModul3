69be82604a5e294558b598ab4d064b25ff562b4c
Albert Sebastian <albertsebastian2301@gmail.com>
2026-03-08 22:07:47 +0700

    Menambahkan implementasi kalkulator

--- TP_MODUL3_103022400122/Form1.cs
+++ TP_MODUL3_103022400122/Form1.cs
@@ -2,7 +2,7 @@ namespace TP_MODUL3_103022400122
 {
     public partial class Form1 : Form
     {
-        string masukkan = "";
+        string masukkan_numbers = "";
         int angkapertama = 0;
         bool isPlusCLicked = false;
 
@@ -14,72 +14,72 @@ namespace TP_MODUL3_103022400122
         private void button1_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            if(masukkan != "")
+            if(masukkan_numbers != "")
             {
-                angkapertama = int.Parse(masukkan);
-                masukkan = "";
+                angkapertama = int.Parse(masukkan_numbers);
+                masukkan_numbers = "";
                 isPlusCLicked = true;
                 textBox1.Text = "+";
 
@@ -89,18 +89,18 @@ namespace TP_MODUL3_103022400122
         private void button11_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            masukkan += btn.Text;
-            textBox1.Text = masukkan;
+            masukkan_numbers += btn.Text;
+            textBox1.Text = masukkan_numbers;
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            if(isPlusCLicked && masukkan != "")
+            if(isPlusCLicked && masukkan_numbers != "")
             {
-                int angkakedua = int.Parse(masukkan);
+                int angkakedua = int.Parse(masukkan_numbers);
                 int hasil = angkapertama + angkakedua;
                 textBox1.Text = hasil.ToString();
-                masukkan = hasil.ToString() + masukkan;
+                masukkan_numbers = hasil.ToString() + masukkan_numbers;
                 isPlusCLicked = false;
             }
         }
