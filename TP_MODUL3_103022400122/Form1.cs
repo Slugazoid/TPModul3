5853a1f6d378d4f5762ba8582821158f5aba065d
Albert Sebastian <albertsebastian2301@gmail.com>
2026-03-08 22:00:29 +0700

    Menambahkan tampilan GUI kalkulator

--- TP_MODUL3_103022400122/Form1.cs
+++ TP_MODUL3_103022400122/Form1.cs
@@ -2,9 +2,107 @@ namespace TP_MODUL3_103022400122
 {
     public partial class Form1 : Form
     {
+        string masukkan = "";
+        int angkapertama = 0;
+        bool isPlusCLicked = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if(masukkan != "")
+            {
+                angkapertama = int.Parse(masukkan);
+                masukkan = "";
+                isPlusCLicked = true;
+                textBox1.Text = "+";
+
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            masukkan += btn.Text;
+            textBox1.Text = masukkan;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if(isPlusCLicked && masukkan != "")
+            {
+                int angkakedua = int.Parse(masukkan);
+                int hasil = angkapertama + angkakedua;
+                textBox1.Text = hasil.ToString();
+                masukkan = hasil.ToString() + masukkan;
+                isPlusCLicked = false;
+            }
         }
     }
 }
--- TP_MODUL3_103022400122/obj/Debug/net10.0-windows/TP_MODUL3_103022400122.AssemblyInfo.cs
+++ TP_MODUL3_103022400122/obj/Debug/net10.0-windows/TP_MODUL3_103022400122.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL3_103022400122")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac20b90db249f3ffdd4e7a560f2898c1b9f5ab01")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL3_103022400122")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL3_103022400122")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
